bf45530660195334a73a3c04311de28261254c9e
sohamsinh31 <sohamb2019@gmail.com>
2022-08-31 15:53:59 +0530

    2.0

--- vplay_python/settings.py
+++ vplay_python/settings.py
@@ -16,12 +16,7 @@ import corsheaders
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sohamb2019@gmail.com'
-EMAIL_HOST_PASSWORD = 'cujdqkbfubeikonn'
-EMAIL_USE_TLS = True
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
